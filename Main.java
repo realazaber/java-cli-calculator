8a0911bb65fe4dff927fe5e433be34097bf30c15
therealcoolpup <azcooldude8@gmail.com>
2021-06-29 05:14:10 +1000

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,40 +5,23 @@ public class Main {
 
         ArrayList<String> _equation = new ArrayList<String>();
 
+        
+
         _equation.add("5");
         _equation.add("+");
         _equation.add("4");
 
+        int _counter = 0;
+
+        boolean _operator = false;
+        boolean _number = false;
+
+        double _result = 0;
+
         for (var _item : _equation) {
-            switch (_item) {
-                case "1":
-                case "2":
-                case "3":
-                case "4":
-                case "5":
-                case "6":
-                case "7":
-                case "8":
-                case "9":
-                case "0":
-                    System.out.println("Number " + _item);
-                    break;
-                case "+":
-                    System.out.println("Addition " + _item);
-                    break;
-                case "-":
-                    System.out.println("Substraction " + _item);
-                    break;
-                case "*":
-                    System.out.println("Multiplication " + _item);
-                    break;
-                case "/":
-                    System.out.println("Division " + _item);
-                    break;
-                default:
-                    System.out.println("Invalid " + _item);
-                    break;
-            }
+            
+
+            _counter++;
         }
 
     }
